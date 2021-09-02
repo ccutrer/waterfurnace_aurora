7c95554dbc5d79c2021bc01069469742ced6a345
Cody Cutrer <cody@cutrer.us>
2021-09-02 09:46:01 -0600

    fix writing humidistat settings
    it broke when I added non-IZ2 support

--- lib/aurora/humidistat.rb
+++ lib/aurora/humidistat.rb
@@ -42,9 +42,10 @@ module Aurora
       @dehumidifer_running = dehumidifier? && outputs.include?(:accessory2)
 
       base = abc.iz2? ? 31_109 : 12_309
-      @humidifier_settings = registers[abc.iz2? ? 21_114 : 12_309]
-      @humidifier_mode = @humidifier_settings&.include?(:auto_humidification) ? :auto : :manual
-      @dehumidifier_mode = @humidifier_settings&.include?(:auto_dehumidification) ? :auto : :manual
+      humidifier_settings_register = abc.iz2? ? 21_114 : 12_309
+      @humidifier_settings = registers[humidifier_settings_register]&.last&.[](2..)&.to_i(16)
+      @humidifier_mode = registers[humidifier_settings_register]&.include?(:auto_humidification) ? :auto : :manual
+      @dehumidifier_mode = registers[humidifier_settings_register]&.include?(:auto_dehumidification) ? :auto : :manual
 
       @humidification_target = registers[base + 1]&.[](:humidification_target)
       @dehumidification_target = registers[base + 1]&.[](:dehumidification_target)
@@ -64,7 +65,7 @@ module Aurora
 
       # start with the prior value of the register, since I'm not sure what
       # else is stuffed in there
-      raw_value = @humidifier_settings &= ~0xc000
+      raw_value = @humidifier_settings
       raw_value |= 0x4000 if humidifier_mode == :auto
       raw_value |= 0x8000 if dehumidifier_mode == :auto
       holding_registers[abc.iz2? ? 21_114 : 12_309] = raw_value
@@ -84,7 +85,7 @@ module Aurora
       raise ArgumentError unless (15..50).include?(humidification_target)
       raise ArgumentError unless (35..65).include?(dehumidification_target)
 
-      holding_registers[iz2? ? 21_115 : 12_310] = (humidification_target << 8) + dehumidification_target
+      holding_registers[abc.iz2? ? 21_115 : 12_310] = (humidification_target << 8) + dehumidification_target
       @humidification_target = humidification_target
       @dehumidification_target = dehumidification_target
     end
