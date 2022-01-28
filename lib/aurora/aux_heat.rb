2f194a0a845104881a5ae5744ebe0be428909547
Cody Cutrer <cody@cutrer.us>
2022-01-28 15:30:19 -0700

    fix layout of aux heat component

--- lib/aurora/aux_heat.rb
+++ lib/aurora/aux_heat.rb
@@ -3,19 +3,19 @@
 require "aurora/component"
 
 module Aurora
-class AuxHeat < Component
+  class AuxHeat < Component
     attr_reader :stage, :watts
 
     def refresh(registers)
-    outputs = registers[30]
-    @stage = if outputs.include?(:eh2)
-                2
-                elsif outputs.include?(:eh1)
-                1
-                else
-                0
-                end
-    @watts = registers[1151] if abc.energy_monitoring?
+      outputs = registers[30]
+      @stage = if outputs.include?(:eh2)
+                 2
+               elsif outputs.include?(:eh1)
+                 1
+               else
+                 0
+               end
+      @watts = registers[1151] if abc.energy_monitoring?
     end
   end
 end
